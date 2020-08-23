2f3fc20bf387705d332a40f9526a470308459872
hollanddd <me@darrenholland.com>
2020-08-23 08:31:20 -0500

    Improve readability

--- lib/deployment-pipeline-stack.ts
+++ lib/deployment-pipeline-stack.ts
@@ -7,26 +7,27 @@ export class DeploymentPipelineStack extends cdk.Stack {
   constructor(app: cdk.App, id: string, props?: cdk.StackProps) {
     super(app, id, props);
 
-    // A code pipeline consists a source (github|codecommit|s3) and one or more
-    // codebuild pipeline projects
+    // A code pipeline consists of a source (github|codecommit|s3) and one or
+    // more build projects
 
-    // A build project configures build commands and execution environment
-    const buildProject = new codebuild.PipelineProject(this, 'BuildPipeline', {
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '0.2',
-        phases: {
-          install: {
-            commands: 'make install',
-          },
-          build: {
-            comands: 'make build',
-          },
-          artifacts: {
-            'base-directory': '$CODEBUILD_SRC_DIR/dist',
-            files: ['./*'],
-          },
+    // The buildSpec configures the phases and commands for the project
+    // Use `.fromSourceFileName` to retrive from local file
+    const buildSpec = codebuild.BuildSpec.fromObject({
+      version: '0.2',
+      phases: {
+        build: {
+          comands: 'make',
         },
-      }),
+        artifacts: {
+          'base-directory': '$CODEBUILD_SRC_DIR/build',
+          files: ['./*'],
+        },
+      },
+    });
+
+    // A build project configures build commands and the execution environment
+    const buildProject = new codebuild.PipelineProject(this, 'BuildPipeline', {
+      buildSpec,
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
       },
